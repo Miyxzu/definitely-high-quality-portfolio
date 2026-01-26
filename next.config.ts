b138250ca0fdf9c86441825345bbc34b47fd508f
Exsu <ezio.hasibuan@gmail.com>
2026-01-26 19:43:35 +0000

    Add changes to allow deployment of page to GH Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  output: "export",
+
+  basePath: "/definitely-high-quality-portfolio",
+
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
