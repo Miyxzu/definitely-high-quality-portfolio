d5fb63d74bcb567cd17b12aef4d726ab66b07d9c
Exsu <ezio.hasibuan@gmail.com>
2026-01-26 21:00:32 +0000

    Additional Changes for Development

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 
-  output: "export",
-
   basePath: "/definitely-high-quality-portfolio",
 
+  output: "export",
+
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
